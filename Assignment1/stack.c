f2dd624cf01af7ab4542e6d7ae4837f039ad159a
Falko <falko.becker@informatik.hu-berlin.de>
2018-04-25 23:06:09 +0200

    basic shit, nothing works

--- Assignment1/stack.c
+++ Assignment1/stack.c
@@ -1,1 +1,39 @@
 #include "stack.h"
+#include <stdlib.h>
+
+
+int stackInit(intstack_t *stack) {
+    stack = NULL;
+    return 1;
+}
+
+void stackRelease(intstack_t *stack) {
+    struct intstack_t *temp = post;
+
+    if (previous == NULL)
+        return;
+    post = post->previous;
+    free(temp);
+}
+
+void stackPush(intstack_t *stack, int i){
+
+}
+
+int stackTop(const intstack_t *stack){
+
+    return 1;
+}
+
+int stackPop(intstack_t *stack){
+    return 1;
+}
+
+
+int stackIsEmpty(const intstack_t *stack){
+    return 1;
+}
+
+void stackPrint(const intstack_t *stack){
+
+}
